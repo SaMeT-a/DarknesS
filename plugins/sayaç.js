5ad29171373e4e7a095b7926931dad43192cd8c8
SaMeT <89554489+SaMeT-a@users.noreply.github.com>
2022-09-26 16:38:27 +0300

    Add files via upload

---
+++ plugins/sayaç.js
@@ -0,0 +1,26 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 30, 2022 13:45:10").getTime();
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+   
+  var distance = countDownDate - now;
+    
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("sayaç").innerHTML = days + ":" + hours + ":"
+  + minutes + ":" + seconds + "";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("sayaç").innerHTML = "AÇILIYORUZ";
+  }
+}, 1000);
